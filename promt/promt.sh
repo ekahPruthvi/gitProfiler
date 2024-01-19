871dbc2b0597516947da83f92ec35ffb4a4a3656
ekahPruthvi <136414172+ekahPruthvi@users.noreply.github.com>
2024-01-19 09:06:28 +0530

    Add files via upload

---
+++ promt/promt.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+clear
+echo "   ▓▓▓▓▓▓─┐
+  ▓▓ ┌──▓▓└┐ ▓▓▓▓▓▓
+  └──┘  ▓▓ │▓▓┌──┐▓▓┐
+        ▓▓ │▓▓│  └──┘
+        ▓▓ │▓▓│
+  ▓▓    ▓▓ │▓▓│
+  └▓▓▓▓▓▓ ┌┘▓▓│   ▓─┐ ▓▓
+   └──────┘  ▓▓▓▓▓▓┌┘ ▓▓┐
+              └────┘   └┘" | pv -qL 90 && sleep 2s
+clear
+echo "Booting cynageOS..." | pv -qL 50 && sleep 1s
+
+clear
+echo "Logging into https://github.com/ekahPruthvi.git ...........
+Profile located....
+Locking target ----->X<------" | pv -qL 50
+
+clear
+echo "
+▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+█░▄▄█░█▀█░▄▄▀█░█████░▄▄░█░▄▄▀█░██░█▄░▄█░████▀███▀██▄███
+█░▄▄█░▄▀█░▀▀░█░▄▄░██░▀▀░█░▀▀▄█░██░██░██░▄▄░██░▀░███░▄██
+█▄▄▄█▄█▄█▄██▄█▄██▄██░████▄█▄▄██▄▄▄██▄██▄██▄███▄███▄▄▄██
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀" | pv -qL 200
+
+kitten icat --align left ~/Downloads/git.jpg && sleep 3s
+echo "Starting [init systems]............[ok]
+Writing to fill disk space ............[OK]
+Reaching out 192.0.163.923..............[ERROR]
+Running [ERRORFIX]
+Reaching https://github.com/ekahPruthvi/error.sh.........[ok]
+Running error.sh.............[ok]
+Reaching out 192.0.163.923............[ok]
+Running profile.git ................[ok]" | pv -qL 30 && sleep 2s
+
+clear
+echo "◕‿↼ ~Hi, I am ekah~
+Welcome to my profile
+
+I am a student at RVITM, Bangalore 
+studying BE CSE 
+
+▒Currently on-going Projects 
+▒╺ cynageOS (https://github.com/ekahPruthvi/cynageOS/.git) 
+
+▒Learning right now 
+▒╺ GTK for ags
+▒╺ RUST programming language
+
+▒Languages and Tools 
+▒╺ Audrino 
+▒╺ Matlab
+▒╺ Figma
+▒╺ C/C++
+▒╺ Linux
+▒╺ Blender 
+
+ping me @twitter.com/ekahPruthvi
+" | pv -qL 30 && sleep 2s
+
+clear
+
